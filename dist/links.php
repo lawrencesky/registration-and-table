8d7867e7bab39a63fa0f90030a7fba2592cfe6fb
FalcasantosKrizelleMae <bg201803214@wmsu.edu.ph>
2022-02-28 12:53:36 +0800

    First

---
+++ dist/links.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="./dist/output.css" />
+<link rel="stylesheet" href="./dist/css/all.css" />
+<!-- UIkit CSS -->
+<link
+   rel="stylesheet"
+   href="https://cdn.jsdelivr.net/npm/uikit@3.11.1/dist/css/uikit.min.css"
+/>
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.11.1/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.11.1/dist/js/uikit-icons.min.js"></script>
+
+<!-- Box icons -->
+<link
+   href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css"
+   rel="stylesheet"
+/>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   content: ['./**/*.{html,js}', './pages/*.{html,js}'],
+   theme: {
+      extend: {
+         colors: {
+            pink: '#A42D2D',
+         },
+         container: {
+            center: true,
+         },
+      },
+   },
+   plugins: [],
+};
